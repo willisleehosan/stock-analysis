e5eab9dbfac7758cf3472e7d5792d727db803fc7
nwillu3993 <69249917+willisleehosan@users.noreply.github.com>
2025-06-23 17:11:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,10 +163,9 @@ def season(df, marketDf, sma):
     ssX.append(np.array([]))
     ssY.append(np.array(group["residue"].values))
     grouped2 = group.groupby(group.index.to_period("M"))
+    st.write(grouped2)
     for name2, group2 in grouped2:
       ssX[len(ssX)-1] = np.append(ssX[len(ssX)-1], np.arange(int(str(name2).split("-")[1]), int(str(name2).split("-")[1])+1, 1.0/group2.size))
-  st.write(ssX)
-  st.write(ssY)
   return ssX, ssY
 # ----------------------------------------------
 ticker = st.text_input("Ticker", "0189") + ".HK"
